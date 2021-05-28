ef5f5520f5955461e798269ad5b758ab4050ec21
Jdoublee <hyewon3429@gmail.com>
2021-05-28 19:56:50 +0900

    swiftui tutorials

---
+++ appPractice/tutorial/tutorial/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  tutorial
+//
+//  Created by hay on 2021/05/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Turtle Rock")
+                .font(.title)
+            HStack {
+                Text("Joshua Tree National Park")
+                    .font(.subheadline)
+                Spacer()
+                Text("California")
+            }
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ appPractice/tutorial/tutorial/tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  tutorialApp.swift
+//  tutorial
+//
+//  Created by hay on 2021/05/28.
+//
+
+import SwiftUI
+
+@main
+struct tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
