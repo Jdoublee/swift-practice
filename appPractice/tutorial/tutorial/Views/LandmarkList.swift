f1e9035bd5eeea6e12a1d79107522890c3a252de
Jdoublee <hyewon3429@gmail.com>
2021-06-20 23:23:41 +0900

    ~ Section 5

---
+++ appPractice/tutorial/tutorial/Views/LandmarkList.swift
@@ -0,0 +1,22 @@
+//
+//  LandmarkList.swift
+//  tutorial
+//
+//  Created by hay on 2021/06/20.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List(landmarks) { landmark in
+            LandmarkRow(landmark: landmark)
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
