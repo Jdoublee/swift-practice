1242982c13e36f8b36b1f95c06622e7aa6d79355
Jdoublee <hyewon3429@gmail.com>
2021-07-09 22:25:58 +0900

    Interfacing with UIKit ~ Section 1

---
+++ appPractice/tutorial/tutorial/Views/PageView/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  tutorial
+//
+//  Created by hay on 2021/07/09.
+//
+
+import Foundation
---
+++ appPractice/tutorial/tutorial/Views/PageViewController.swift
@@ -0,0 +1,20 @@
+//
+//  PageViewController.swift
+//  tutorial
+//
+//  Created by hay on 2021/07/09.
+//
+
+import SwiftUI
+
+struct PageViewController: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PageViewController_Previews: PreviewProvider {
+    static var previews: some View {
+        PageViewController()
+    }
+}
