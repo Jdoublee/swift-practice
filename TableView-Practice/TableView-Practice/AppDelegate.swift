d42a37ea47d5bc63e1d46b38281665295382df62
Jdoublee <hyewon3429@gmail.com>
2022-03-18 00:10:13 +0900

    TableView-Practice 프로젝트 생성 및 스플래시 화면 작성

---
+++ TableView-Practice/TableView-Practice/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  TableView-Practice
+//
+//  Created by Jdoublee on 2022/03/17.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    //var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        sleep(2) // 스플래시 이미지 딜레이 주기
+        return true
+    }
+
+
+}
+
---
+++ TableView-Practice/TableView-Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableView-Practice
+//
+//  Created by Jdoublee on 2022/03/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
