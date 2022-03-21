e810ca7c8ec322f26a9cf891d1d5cc4fa492a22d
Jdoublee <hyewon3429@gmail.com>
2022-03-21 21:46:59 +0900

    주석처리 (테스트용)

--- appPractice/TableView-Practice/TableView-Practice/AppDelegate.swift
+++ appPractice/TableView-Practice/TableView-Practice/AppDelegate.swift
@@ -10,11 +10,11 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    //var window: UIWindow?
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        sleep(2) // 스플래시 이미지 딜레이 주기
+        //sleep(2) // 스플래시 이미지 딜레이 주기
         return true
     }
 
