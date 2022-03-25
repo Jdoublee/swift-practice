ba8c4622550285597f2a85b28705e69b689d56dc
Jdoublee <hyewon3429@gmail.com>
2022-03-25 23:51:23 +0900

    영화인 검색을 위한 테이블셀 및 VO 생성

---
+++ appPractice/TableView-Practice/TableView-Practice/PeopleCell.swift
@@ -0,0 +1,16 @@
+//
+//  PeopleCell.swift
+//  TableView-Practice
+//
+//  Created by Jdoublee on 2022/03/23.
+//
+
+import UIKit
+
+class PeopleCell: UITableViewCell {
+    
+    @IBOutlet var peopleName: UILabel!
+    @IBOutlet var peopleNameEng: UILabel!
+    @IBOutlet var roleName: UILabel!
+    @IBOutlet var filmoNames: UILabel!
+}
---
+++ appPractice/TableView-Practice/TableView-Practice/PeopleVO.swift
@@ -0,0 +1,16 @@
+//
+//  PeopleVO.swift
+//  TableView-Practice
+//
+//  Created by Jdoublee on 2022/03/23.
+//
+
+import Foundation
+
+class PeopleVO {
+    var peopleCd: String?
+    var peopleNm: String?
+    var peopleNmEn: String?
+    var repRoleNm: String?
+    var filmoNames: String?
+}
